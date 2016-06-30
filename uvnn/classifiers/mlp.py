70144f539366c4e30880fbc4a2c74554a89f3924
Zura Isakadze <z.isakadze@gmail.com>
2016-06-30 13:54:12 +0200

    Add general MLP class
    Add MLP which works for any dimensions, other classes are still left
    as they might be simpler to read (becasue the variables are named explicitely
    W x + b1, U h1 + b2 and cycles aren't used ... )

--- uvnn/classifiers/mlp.py
+++ uvnn/classifiers/mlp.py
@@ -1,5 +1,8 @@
+# General multi layer pereptron for anu number of layers
+
 from nn.base import NNBase
-from nn.math import softmax, make_onehot, tanh, tanhd
+from nn.math import softmax, make_onehot, tanh, tanhd, sigmoid_grad, sigmoid
+
 from misc import random_weight_matrix
 import numpy as np
 
@@ -9,9 +12,9 @@ class MLP(NNBase):
     This implements softmax regression, trained by SGD.
     """
 
-    def __init__(self, dims=[100, 5],
+    def __init__(self, dims=[100, 30, 20, 5],
                  reg=0.1, alpha=0.001,
-                 rseed=10):
+                 rseed=10, activation='tanh'):
         """
         Set up classifier: parameters, hyperparameters
         """
@@ -19,68 +22,138 @@ class MLP(NNBase):
         # Store hyperparameters
         self.lreg = reg # regularization
         self.alpha = alpha # default learning rate
-        self.nclass = dims[1] # number of output classes
+        self.nclass = dims[-1] # number of output classes
         self.dims = dims # todo move to superclass
+        self.outputsize = dims[-1]
 
+        
+        ## We name the parameters as following 
+        # W1, b1, W2, b2, W3, b3 ... 
+        param_dims = {}
+        for i in range(1, len(dims)):
+            w_param = 'W' + str(i)
+            b_param = 'b' + str(i)
+            param_dims[w_param] = (dims[i], dims[i-1])
+            param_dims[b_param] = (dims[i], )
 
-        param_dims = dict(W=(dims[1], dims[0]),
-                          b1=(dims[1],),
-                          U=(dims[2], dims[1]),
-                          b2=(dims[2],),
-                          )
         NNBase.__init__(self, param_dims)
 
+        # set activation function
+        if activation =='tanh':
+            self.act = tanh
+            self.act_grad = tanhd
+        elif activation == 'sigmoid':
+            self.act = sigmoid
+            self.act_grad = sigmoid_grad
+        else:
+            raise 'Uknown activation function'
+
         #self.sparams.L = wv.copy() # store own representations
-        self.params.W = random_weight_matrix(*self.params.W.shape)
-        self.params.U = random_weight_matrix(*self.params.U.shape)
-        self.outputsize = dims[2]
+        # init weights
+        for i in range(1, len(self.dims)):
+            self._set_param('W', i, random_weight_matrix(dims[i], dims[i -1]))
+
+    def _set_param(self, param_name, ind, val):
+        ''' set parameters
+            param name : b or W
+            ind : number of parameter W1, b1 (first layer) W2, b2, ..
+            val : value to set
+        '''
+        full_name = param_name + str(ind)
+        self.params.__setitem__(full_name, val)
+
+    def _get_param(self, param_name, ind):
+        ''' get parameters
+            param name : b or W
+            ind : number of parameter W1, b1 (first layer) W2, b2, ..
+        '''
+        full_name = param_name + str(ind)
+        return self.params.__getitem__(full_name)
+
+    def _add_grads(self, grad_name, ind, val):
+        ''' Add gradient to correspoding param '''
+        full_name = grad_name + str(ind)
+        old = self.grads.__getitem__(full_name)
+        self.grads.__setitem__(full_name, old + val)
+
 
     def forward_pass(self, x):
-        z1 = self.params.W.dot(x) + self.params.b1
-        h = tanh(z1)
-        z2 = np.dot(self.params.U, h) + self.params.b2
-        y_hat = softmax(z2)
-        return y_hat
+        ''' Forward pass,
+        Arguemnt: x input vectore
+        Return:(zs, hs) hidden and output activations (hs)
+        and inputs to activation function (zs) 
 
+        example:
+            input _ dims [100, 30, 20, 5]
+            output: hs = [x, h1, h2, h3]
+                    zs = [z1, z2, z3]
+        '''
+
+        hs = [x]
+        zs = []
+        h = x
+        for i in range(1, len(self.dims)):
+            W = self._get_param('W', i)
+            b = self._get_param('b', i)
+            z = W.dot(h) + b
+            zs.append(z)
+            # now activation function, if it's a last layer we use softmax
+            # else tanh
+            if i == len(self.dims) - 1:
+                # last layer
+                h = softmax(z)
+            else:
+                h = self.act(z)
+            hs.append(h)       
+        return (zs, hs)
 
     def _acc_grads(self, x, label):
         """
         Accumulate gradients from a training example.
         """
+        #import ipdb; ipdb.set_trace()
         ##
-        # Forward propagation
-        z1 = self.params.W.dot(x) + self.params.b1
-        h = tanh(z1)
-        z2 = np.dot(self.params.U, h) + self.params.b2
-        y_hat = softmax(z2)
+        # Forward Pass 
+        zs, hs = self.forward_pass(x)
         
+        y_hat = hs[-1]
         y = make_onehot(label, self.outputsize)
-        d2 = y_hat - y
-        self.grads.b2 += d2
-        self.grads.U += np.outer(d2, h) + self.lreg * self.params.U
-        d1 = np.dot(self.params.U.T, d2) * tanhd(z1) 
+        delta = y_hat - y
         
-        self.grads.W += np.outer(d1, x) + self.lreg * self.params.W
-        self.grads.b1 += d1
+        cur_h = len(hs) -2 # current h vector index
+        cur_z = len(zs) -2 # current z vector index 
+        # Backpropagation 
+        #import ipdb; ipdb.set_trace()
+        for i in range(len(self.dims) - 1, 0, -1):
+            self._add_grads('b', i, delta)
+            curw = self._get_param('W', i)
+            gradw = np.outer(delta, hs[cur_h])
+            gradw_reg = self.lreg * curw
+            self._add_grads('W', i, gradw + gradw_reg)
+            if cur_z >= 0:
+                delta = np.dot(curw.T, delta) * self.act_grad(zs[cur_z])
+            cur_h -= 1
+            cur_z -= 1
 
 
     def compute_loss_full(self, X, y):
-        #import ipdb; ipdb.set_trace()
+        ''' compute cross entropy loss for all examples in X
+        '''
         loss = 0
         for idx in xrange(X.shape[0]):
-            loss += self.compute_loss(X[idx], y[idx])
-        Jreg = (self.lreg / 2.0) * (np.sum(self.params.W**2.0) + 
-                np.sum(self.params.U**2.0))
+            x = X[idx]
+            label = y[idx]
+            zs, hs = self.forward_pass(x)
+            y_hat = hs[-1] # last layer is a final output
+            loss -= np.log(y_hat[label])
+            
+        # now compute Jreg
+        Jreg = 0
+        for i in range(1, len(self.dims)):
+            W = self._get_param('W', i)
+            Jreg += np.sum(W**2.)
+        Jreg = self.lreg / 2. * Jreg
         return loss + Jreg
-
-
-    def compute_loss(self, x, label):
-        """
-        Compute the cost function for a single example.
-        """
-        # Forward propagation
-        y_hat = self.forward_pass(x)
-        return -np.log(y_hat[label])
     
     def predict_proba(self, X):
         # we can guess numb of class by params.b
@@ -90,8 +163,8 @@ class MLP(NNBase):
         """
         Predict class probabilities.
         """
-        p = self.forward_pass(x)
-        return p
+        zs, hs = self.forward_pass(x)
+        return hs[-1]
     
     def predict(self, X):
         return np.apply_along_axis(self.predict_single, 1, X)
@@ -102,6 +175,13 @@ class MLP(NNBase):
         return np.argmax(P)
     
     def get_weights(self):
-        W1 = np.hstack([self.params.W, self.params.b1.reshape(-1, 1)])
-        W2 = np.hstack([self.params.U, self.params.b2.reshape(-1, 1)])
-        return (W1, W2)
+        ''' Get list of weights of each matrix, the baias terms are appended
+        as the last column '''
+        ret_ws = []
+        for i in range(1, len(self.dims)):
+            W = self._get_param('W', i)
+            b = self._get_param('b', i)
+            stacked = np.hstack([W, b.reshape(-1, 1)])
+            ret_ws.append(stacked)
+
+        return ret_ws
--- uvnn/classifiers/mlp_2layer.py
+++ uvnn/classifiers/mlp_2layer.py
@@ -9,7 +9,7 @@ class MLP(NNBase):
     This implements softmax regression, trained by SGD.
     """
 
-    def __init__(self, dims=[100, 20, 20, 5],
+    def __init__(self, dims=[100, 5, 3],
                  reg=0.1, alpha=0.001,
                  rseed=10):
         """
@@ -27,24 +27,19 @@ class MLP(NNBase):
                           b1=(dims[1],),
                           U=(dims[2], dims[1]),
                           b2=(dims[2],),
-                          G = (dims[3], dims[2]),
-                          b3 = (dims[3], ),
                           )
         NNBase.__init__(self, param_dims)
 
         #self.sparams.L = wv.copy() # store own representations
         self.params.W = random_weight_matrix(*self.params.W.shape)
         self.params.U = random_weight_matrix(*self.params.U.shape)
-        self.params.G = random_weight_matrix(*self.params.G.shape)
-        self.outputsize = dims[3]
+        self.outputsize = dims[2]
 
     def forward_pass(self, x):
         z1 = self.params.W.dot(x) + self.params.b1
-        h1 = tanh(z1)
-        z2 = np.dot(self.params.U, h1) + self.params.b2
-        h2 = tanh(z2)
-        z3 = np.dot(self.params.G, h2) + self.params.b3
-        y_hat = softmax(z3)
+        h = tanh(z1)
+        z2 = np.dot(self.params.U, h) + self.params.b2
+        y_hat = softmax(z2)
         return y_hat
 
 
@@ -52,25 +47,17 @@ class MLP(NNBase):
         """
         Accumulate gradients from a training example.
         """
-        #import ipdb; ipdb.set_trace()
         ##
         # Forward propagation
         z1 = self.params.W.dot(x) + self.params.b1
-        h1 = tanh(z1)
-        z2 = np.dot(self.params.U, h1) + self.params.b2
-        h2 = tanh(z2)
-        z3 = np.dot(self.params.G, h2) + self.params.b3
-        y_hat = softmax(z3)
+        h = tanh(z1)
+        z2 = np.dot(self.params.U, h) + self.params.b2
+        y_hat = softmax(z2)
         
         y = make_onehot(label, self.outputsize)
-        d3 = y_hat - y
-        self.grads.b3 += d3
-        self.grads.G += np.outer(d3, h2) + self.lreg * self.params.G
-
-        d2 = np.dot(self.params.G.T, d3) * tanhd(z2)
-
+        d2 = y_hat - y
         self.grads.b2 += d2
-        self.grads.U += np.outer(d2, h1) + self.lreg * self.params.U
+        self.grads.U += np.outer(d2, h) + self.lreg * self.params.U
         d1 = np.dot(self.params.U.T, d2) * tanhd(z1) 
         
         self.grads.W += np.outer(d1, x) + self.lreg * self.params.W
@@ -83,7 +70,7 @@ class MLP(NNBase):
         for idx in xrange(X.shape[0]):
             loss += self.compute_loss(X[idx], y[idx])
         Jreg = (self.lreg / 2.0) * (np.sum(self.params.W**2.0) + 
-                np.sum(self.params.U**2.0) + np.sum(self.params.G**2.))
+                np.sum(self.params.U**2.0))
         return loss + Jreg
 
 
@@ -117,6 +104,4 @@ class MLP(NNBase):
     def get_weights(self):
         W1 = np.hstack([self.params.W, self.params.b1.reshape(-1, 1)])
         W2 = np.hstack([self.params.U, self.params.b2.reshape(-1, 1)])
-        W3 = np.hstack([self.params.G, self.params.b3.reshape(-1, 1)])
-
-        return (W1, W2, W3)
+        return (W1, W2)
