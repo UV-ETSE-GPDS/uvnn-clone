4f61d00d742d5b9f1b9a007a4f4affc629a3dfa7
Zura Isakadze <z.isakadze@gmail.com>
2016-07-14 18:35:09 +0200

    Refactor simple SNN
    Simplified, commented and mroe efficient implementation, of simplified snn.

--- uvnn/snns/multi_neuron.py
+++ uvnn/snns/multi_neuron.py
@@ -1,98 +1,128 @@
 import numpy as np
 
-time_step = 1 # simulation step second
-simulation_time = 1000 # seconds
-D = 2 # potential degradation value
+conf = {'time_step': 2, # simulation step second
+       'simulation_time':300, # seconds
+        'D':1.5, # potential degradation value
+        'p_max':0.5,
+        'p_min':-10,
+        'p_rest':0,
+        't_refr':9,
+        'n_input':4,
+        'n_output':2,
+        'spike_periods':[1, 20, 40, 60],
+        'w_max':2,
+        'w_min':-1,
+        'sigma':0.4,
+        'margin':2,
+        'windowsize':5,
+        'tau_plus':3,
+        'tau_minus':3,
+        'a_plus':2,
+        'a_minus':-2,
+       }
 
-p_max = 10
-p_min = -10
-p_rest = 0
-a_plus = 2
-a_minus = 2
-tau_plus = 3
-tau_minus = 3
+def get_presynaptics(time, spike_periods):
+    ''' Get which neurons are firing on provided time
 
+    Right now we feed spike_freq but it can be array of spikes trains later
 
-t_refr = 10
+    Args: 
+        time: current clock cycle
+        spike_periods: numpy array of preiod of each spikes
+    Return:
+        bool array of currently active active neurons
+    '''
+    active = []
+    for i, period in enumerate(spike_periods):
+        if time % period == 0:
+            active.append(i)
 
-# simple snn with 1 membrane:
+    return active
 
-n_input = 4
-spike_freq = [10, 15, 35, 40]
-n_output = 2
-windowsize = 5
-margin = 2
-sigma = 0.4
-w_max = 2
-w_min = -1
 
 def simulate(time_step, simulation_time, D, p_max, p_min, p_rest, t_refr, 
-        n_input, spike_freq, n_output, tau_plus, tau_minus, a_plus, a_minus):
-
-    traces_all = [[p_rest] for _ in range(n_output)] # membranes potentials
-    
-    out_spikes_all = [set() for _ in range(n_output)] 
-    # we keep the times when each neuron fires spikes 
-
-    # initialize weights
-    weights = np.ones((n_input, n_output))
-
-
-    for time in range(0, simulation_time, time_step):
-        for post_neuron in range(n_output):
-            p_cur = traces_all[post_neuron][-1] #current potentials
-            out_spikes = out_spikes_all[post_neuron]
-            trace = traces_all[post_neuron]
+        n_input, spike_periods, n_output, tau_plus, tau_minus, a_plus, a_minus,
+        w_max, w_min, windowsize, margin, sigma):
         
+    init_potentials = np.zeros(n_output)
+    init_potentials.fill(p_rest)
 
-            if len(out_spikes) > 0 and time - out_spikes[-1] < t_refr:
-                # don't do anything
-                trace.append(p_cur)
-                continue
-
-            if p_cur > p_max:
-                # Fire, Spike happened in post synaptic
-                out_spikes.append(time)
-                # update weights
-                
-                # calculate dw for presynaptic spikes
-                for pre_time in range(time - windowsize, time - margin):
-                    delta_time = time - pre_time
-                    for pre_neuron in range(n_input):
-                        if pre_time % spike_freq[pre_neuron] == 0:
-                            dw = a_plus * np.exp(delta_time / float(tau_plus))
-
-                            # update weights
-                            w_old = weights[pre_neuron][post_neuron]
-                            weights[pre_neuron][post_neuron] += (sigma * dw * 
-                                    (w_max - w_old))
-
-
-                p_cur = p_rest
-                trace.append(p_cur)
-                continue
-
-            if p_cur < p_min:
-                p_cur = p_rest
-                trace.append(p_cur)
-                continue
-
-            # increase the potential, incoming spikes
-            for pre_neuron in range(n_input):
-                if time % spike_freq[pre_neuron] == 0: # spike happens
-                    p_cur += weights[pre_neuron][post_neuron]
-
-                    # spike heppened in pre_synaptic neuron
-                    # now update weights considering the postsynaptic 
-                    # spikes which happened before this (negative dependency)
-                    
-                    # Let's just check the last firing times of post synaptic
-
-
-            # now substract D for this time instant
-            if p_cur > p_rest:
-                p_cur -= D
-
-            trace.append(p_cur)
+    traces = [init_potentials] # membranes potentials
     
-    return traces_all, out_spikes_all
+    # all output spikes for each neuron
+    out_spikes = [set() for _ in range(n_output)]
+    
+    # we keep the times when each neuron fires spikes 
+    out_spikes_last = np.zeros(n_output) 
+    out_spikes_last.fill(-10000)
+    
+    weights = np.random.rand(n_input, n_output) # initialize weights
+    
+    spike_periods = np.array(spike_periods) # just convert to numpy array
+
+
+    for time in range(0, simulation_time, time_step): 
+        if time % 10 == 0:
+            print weights
+        #import ipdb; ipdb.set_trace()
+        cur_potentials = traces[-1]
+        new_potentials = np.copy(cur_potentials)
+
+        # fire neurons which are above threshold
+        fired = cur_potentials > p_max
+        for post_neuron in np.nonzero(fired)[0]:
+            out_spikes[post_neuron].add(time)
+            out_spikes_last[post_neuron] = time
+        new_potentials[fired] = p_rest
+
+        # update weights for presynaptic neurons (positively correlated)
+        already_updated = set() # keep already updated neurons not to update twice
+        for pre_time in range(time - windowsize, time - margin):
+            delta_time = time - pre_time
+            pre_neurons = get_presynaptics(pre_time, spike_periods)
+            # distract neurons already updated
+            pre_neurons = list(set(pre_neurons) - already_updated)
+            dw = a_plus * np.exp(delta_time / float(tau_plus))
+            weights[pre_neurons] += sigma * dw * (w_max - weights[pre_neurons])
+            already_updated = already_updated | set(pre_neurons) # unite
+
+        
+        # depolarize strongly negative neurons
+        strongly_negative = cur_potentials < p_max
+        new_potentials[strongly_negative] = p_rest
+
+            
+        #get postsynaptic neurons which  are not in refractory period
+        # and add weights from presynaptic
+        post_neurons = out_spikes_last < time - t_refr
+        
+        # get presynaptic neurons which fires now
+        pre_neurons = get_presynaptics(time, spike_periods)
+
+        for post_neuron in np.nonzero(post_neurons)[0]:
+            for pre_neuron in pre_neurons:
+                new_potentials[post_neuron] += weights[pre_neuron][post_neuron]
+            new_potentials[post_neuron] -= D # decay
+        
+        # now update wieghts for postsyanptic neurons (negatively correlated)
+        alread_updated = set()
+        for pre_time in range(time - windowsize, time - margin):
+            for post_neuron in range(n_output):
+                # check if already updated 
+                if post_neuron in alread_updated:
+                    continue
+                # check if post_neuron was fired before pre_neuron
+                if pre_time in out_spikes[post_neuron]:
+                    delta_time = time - pre_time
+                    dw = a_minus * np.exp(delta_time / float(tau_minus))
+                    #import ipdb; ipdb.set_trace()
+                    weights[pre_neurons, post_neuron] += (sigma * dw * 
+                            (weights[pre_neurons, post_neuron]-w_min))
+                already_updated.add(post_neuron)
+
+        traces.append(new_potentials)  # keep potential trace
+
+    return traces, out_spikes
+
+
+#simulate(**conf)
