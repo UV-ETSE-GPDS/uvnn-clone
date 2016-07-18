745aecf1913d03f387b5ff30152f3645fe413439
Zura Isakadze <z.isakadze@gmail.com>
2016-07-18 19:00:44 +0200

    Add neighbour depression, config params
    Potential of neighbours are now depressed when fired,
    parameters for simple task are fixed to show show learning.

--- uvnn/snns/multi_neuron.py
+++ uvnn/snns/multi_neuron.py
@@ -1,7 +1,8 @@
 import numpy as np
 from collections import defaultdict
+from uvnn.classifiers.misc import random_weight_matrix
 
-sim_time = 400
+sim_time = 600
 frag_time = 100
 def gen_spike(periods = [10, 40], sim_time = 200, frag_time=100):
     # build a spike train with alternating periods
@@ -11,36 +12,38 @@ def gen_spike(periods = [10, 40], sim_time = 200, frag_time=100):
     while (t_now < sim_time):
         fragment = range(t_now, t_now + frag_time, periods[cur_period])
         ret.extend(fragment)
-        t_now += frag_time
         cur_period = (cur_period + 1) % len(periods)        
+        t_now = fragment[-1] + periods[cur_period]
     return ret
 
-train1 = gen_spike([6, 23], sim_time, frag_time)
-train2 = gen_spike([23, 6], sim_time, frag_time)
-train3 = gen_spike([6, 23], sim_time, frag_time)
-train4 = gen_spike([23, 6], sim_time, frag_time)
-spike_trains = [train1, train2, train3, train4]
+train1 = gen_spike([5, 21], sim_time, frag_time)
+train2 = gen_spike([21, 5], sim_time, frag_time)
+train3 = gen_spike([5, 21], sim_time, frag_time)
+train4 = gen_spike([21, 5], sim_time, frag_time)
+spike_trains = [train1, train2]#, train3, train4]
 
 conf = {'time_step': 1, # simulation step second
        'simulation_time':sim_time, # seconds
-        'D':0.3, # potential degradation value
+        'D':0.01, # potential degradation value
         'p_max':3,
         'p_min':-2,
         'p_rest':0,
-        't_refr':5,
+        't_refr':14,
         'n_input':4,
         'n_output':2,
         'spike_trains':spike_trains,
-        'w_max':2,
-        'w_min':-1,
+        'w_max':15,
+        'w_min':-15,
         'sigma':0.1,
-        'margin':2,
-        'windowsize':5,
-        'tau_plus':10,
-        'tau_minus':10,
+        'margin':0,
+        'windowsize':10,
+        'tau_plus':20,
+        'tau_minus':20,
         'a_plus':0.2,
         'a_minus':-0.2,
+        'd_neighbours':0.1
        }
+
 
 def get_presynaptics(time, spike_dict):
     ''' Get which neurons are firing on provided time
@@ -76,7 +79,7 @@ def get_spike_dict(spike_trains):
 
 def simulate(time_step, simulation_time, D, p_max, p_min, p_rest, t_refr, 
         n_input, spike_trains, n_output, tau_plus, tau_minus, a_plus, a_minus,
-        w_max, w_min, windowsize, margin, sigma):
+        w_max, w_min, windowsize, margin, sigma, d_neighbours):
         
     init_potentials = np.zeros(n_output)
     init_potentials.fill(p_rest)
@@ -85,25 +88,34 @@ def simulate(time_step, simulation_time, D, p_max, p_min, p_rest, t_refr,
     
     # all output spikes for each neuron
     out_spikes = [set() for _ in range(n_output)]
-    
+
+
+
     # we keep the times when each neuron fires spikes 
     out_spikes_last = np.zeros(n_output) 
     out_spikes_last.fill(-10000)
-    
-    weights = np.random.rand(n_input, n_output) # initialize weights
-    
+     
+    weights = 2 * np.random.rand(n_input, n_output) # initialize weights
+    #weights = random_weight_matrix(n_input, n_input) 
     # build up a dict which will help us getting spikes on timestep
     spike_dict = get_spike_dict(spike_trains)
     w_trace = []
 
     for time in range(0, simulation_time, time_step): 
-        #import ipdb; ipdb.set_trace()
         w_trace.append(weights.copy()) 
         cur_potentials = traces[-1]
         new_potentials = np.copy(cur_potentials)
 
         # fire neurons which are above threshold
         fired = cur_potentials > p_max
+
+        # depress potentials of neighbours
+        if np.any(fired):
+            #nz = np.where(fired > 0)
+            new_potentials -= d_neighbours
+            new_potentials[fired] += d_neighbours
+
+
         for post_neuron in np.nonzero(fired)[0]:
             out_spikes[post_neuron].add(time)
             out_spikes_last[post_neuron] = time
@@ -113,16 +125,17 @@ def simulate(time_step, simulation_time, D, p_max, p_min, p_rest, t_refr,
         already_updated = set() # keep already updated neurons not to update twice
         if np.any(fired):
             # TODO consider refactor 3 time nesting
+            #import ipdb; ipdb.set_trace()
             for pre_time in range(time - windowsize, time - margin):
                 delta_time = time - pre_time
                 pre_neurons = get_presynaptics(pre_time, spike_dict)
                 # distract neurons already updated
-                #if len(pre_neurons) > 0:
-                #    import ipdb; ipdb.set_trace()
                 pre_neurons = list(set(pre_neurons) - already_updated)
                 if len(pre_neurons) > 0: 
                     dw = a_plus * np.exp(delta_time / float(tau_plus))
-                    weights[pre_neurons, fired] += sigma * dw * (w_max - weights[pre_neurons, fired])
+                    d_increase = sigma * dw * (w_max - weights[pre_neurons, fired])
+                    print 'Increasing ... ', dw, d_increase
+                    weights[pre_neurons, fired] += d_increase
                     already_updated = already_updated | set(pre_neurons) # unite
 
         
@@ -144,27 +157,29 @@ def simulate(time_step, simulation_time, D, p_max, p_min, p_rest, t_refr,
         
         
         # now update wieghts for postsyanptic neurons (negatively correlated)
-        already_updated = set()
-        for pre_time in range(time - windowsize, time - margin):
-            for post_neuron in range(n_output):
-                # check if already updated 
-                if post_neuron in already_updated:
-                    continue
-                # check if post_neuron was fired before pre_neuron
-                if pre_time in out_spikes[post_neuron]:
-                    delta_time = time - pre_time
-                    dw = a_minus * np.exp(delta_time / float(tau_minus))
-                    if np.count_nonzero(post_neurons) > 0:
-                        pass
-                        #import ipdb; ipdb.set_trace()
-                    weights[pre_neurons, post_neuron] += (sigma * dw * 
-                            (weights[pre_neurons, post_neuron]-w_min))
-                already_updated.add(post_neuron)
+        if np.any(pre_neurons):
+            already_updated = set()
+            for pre_time in range(time - windowsize, time - margin):
+                for post_neuron in range(n_output):
+                    # check if already updated 
+                    if post_neuron in already_updated:
+                        continue
+                    # check if post_neuron was fired before pre_neuron
+                    if pre_time in out_spikes[post_neuron]:
+                        delta_time = time - pre_time
+                        dw = a_minus * np.exp(delta_time / float(tau_minus))
+                        d_decrease = sigma * dw * (weights[pre_neurons, post_neuron]-w_min)
+                        print 'Decreasing ...', dw, d_decrease
+                        weights[pre_neurons, post_neuron] += d_decrease
+                    already_updated.add(post_neuron)
         
+
+
         
         # degrade potentials > p_rest
         to_degrade = new_potentials > p_rest
         new_potentials[to_degrade] -= D
+
 
         traces.append(new_potentials)  # keep potential trace
 
